af7e1e0c2e10c6ae0d548d9ff036b92d860343db
FeliCita <felicita2031@gmail.com>
2023-04-27 13:38:34 +0300

    vans sayfası tasarlandı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Vans from './pages/Vans';
+import VanDetail from './pages/VanDetail';
+
+import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
+
+import "./server.js"
 
 function App() {
   return (
-    <div >
-     app  van life
-    </div>
+    <BrowserRouter>
+    <header>
+      <Link className='site-logo' to={"/"}>#VANLIFE</Link>
+    <nav>
+      <Link to={"/about"}>About</Link>
+      <Link to={"/vans"}>Vans</Link>
+    </nav>
+    </header>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/vans" element={<Vans/>}/>
+      <Route path="/vans/:id" element={<VanDetail/>}/>
+    </Routes>
+    </BrowserRouter> 
   );
 }
 
---
+++ src/pages/Vans.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Vans = () => {
+  const [vans, setVans] = useState([]);
+  useEffect(() => {
+    fetch("/api/vans")
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  const vanElements = vans.map((van) => (
+    <div key={van.id} className="van-title">
+      <Link to={`/vans/${van.id}`}>
+        <img src={van.imageUrl} alt="" />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price}</p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </Link>
+    </div>
+  ));
+
+  return (
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <div className="van-list">{vanElements}</div>
+      {vanElements}
+    </div>
+  );
+};
+export default Vans;
